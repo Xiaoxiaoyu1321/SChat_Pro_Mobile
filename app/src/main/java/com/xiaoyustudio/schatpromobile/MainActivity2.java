432160f09aa03b6002da3a5fae0c02df5dd3bb47
Xiaoxiaoyu1321 <120882158+Xiaoxiaoyu1321@users.noreply.github.com>
2023-05-29 07:16:25 +0800

    修改应用展示

--- app/src/main/java/com/xiaoyustudio/schatpromobile/MainActivity2.java
+++ app/src/main/java/com/xiaoyustudio/schatpromobile/MainActivity2.java
@@ -1,8 +1,15 @@
 package com.xiaoyustudio.schatpromobile;
 
+import static android.app.PendingIntent.getActivity;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +17,24 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        TextView textView = findViewById(R.id.Chat);
+        
+
+        SharedPreferences sharedPreferences;
+        sharedPreferences = getSharedPreferences("root_preferences",Context.MODE_PRIVATE);
+        textView.setText(sharedPreferences.getString("密码","nmsl"));
+
+    }
+    public void AddMessage(View view){
+        TextView textView = findViewById(R.id.Chat);
+        EditText editText = findViewById(R.id.SendThings);
+        String aThing = textView.getText().toString();
+        String bThing = editText.getText().toString();
+        String cThing =  aThing  + "self:" + bThing+ "\n";
+        textView.setText(cThing);
+        editText.setText("");
+
+
+
     }
 }
